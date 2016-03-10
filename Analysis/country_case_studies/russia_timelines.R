d6de6624d4ed9914c4302de14153bf979299d3c0
Andrew Heiss <andrew@andrewheiss.com>
2016-03-10 09:27:52 -0500

    Minor adjustments to timeline

--- Analysis/country_case_studies/russia_timelines.R
+++ Analysis/country_case_studies/russia_timelines.R
@@ -4,14 +4,11 @@ library(readr)
 library(lubridate)
 library(ggplot2)
 library(gtable)
-library(ggrepel)
+library(gridExtra)
 library(scales)
 library(Cairo)
 
 source(file.path(PROJHOME, "Analysis", "lib", "graphic_functions.R"))
-
-my.seed <- 1234
-set.seed(my.seed)
 
 
 # Load data
@@ -48,6 +45,8 @@ presidents <- read_csv(file.path(PROJHOME, "Data", "data_base",
 #' # Plot timelines
 plot.csre <- ggplot(russia.data, aes(x=year.actual, y=cs_env_sum)) + 
   geom_line(size=1) + 
+  geom_vline(data=laws, aes(xintercept=as.numeric(Date)),
+             size=0.5, colour="grey50", linetype="dotted") +
   labs(x=NULL, y="CSRE") + 
   coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
   theme_ath()
@@ -55,58 +54,70 @@ plot.csre <- ggplot(russia.data, aes(x=year.actual, y=cs_env_sum)) +
 plot.icrg.internal <- ggplot(icrg.russia, 
                              aes(x=Date, y=icrg.pol.risk.internal.scaled)) +
   geom_line(size=1) + 
-  labs(x=NULL, y="Internal political risk") + 
+  geom_vline(data=laws, aes(xintercept=as.numeric(Date)),
+             size=0.5, colour="grey50", linetype="dotted") +
+  labs(x=NULL, y="Internal risk") + 
   coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
   theme_ath()
 
 plot.icrg.external <- ggplot(russia.data, aes(x=year.actual, 
                                               y=icrg.pol.risk.subregional.loo)) +
   geom_line(size=1) + 
-  labs(x=NULL, y="Mean subregional political risk") + 
+  geom_vline(data=laws, aes(xintercept=as.numeric(Date)),
+             size=0.5, colour="grey50", linetype="dotted") +
+  labs(x=NULL, y="Subregional risk") + 
   coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
   theme_ath()
 
 plot.shaming <- ggplot(russia.data, aes(x=year.actual, y=icews.pct.shame)) +
   geom_line(size=1) + 
-  labs(x=NULL, y="Mean subregional political risk") + 
+  geom_vline(data=laws, aes(xintercept=as.numeric(Date)),
+             size=0.5, colour="grey50", linetype="dotted") +
+  labs(x=NULL, y="% shaming") + 
   coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
   scale_y_continuous(labels=percent) +
   theme_ath()
 
 plot.misc <- ggplot() +
   geom_segment(data=presidents, aes(x=Date_start, xend=Date_end, 
-                                    y=0.5, yend=0.5, colour=president), size=5) +
+                                    y=0.5, yend=0.5, colour=president), size=4) +
   geom_rect(data=wars, aes(x=NULL, y=NULL, xmin=Date_start, xmax=Date_end, 
-                           ymin=0, ymax=0.3, fill=war)) + 
+                           ymin=0, ymax=0.2, fill=war)) + 
+  geom_vline(data=laws, aes(xintercept=as.numeric(Date)),
+             size=0.5, colour="grey50", linetype="dotted") +
   scale_colour_manual(values=c("#EB6742", "#CC333F", "#00A0B0"), name=NULL) + 
   scale_fill_manual(values=c("#E41A1C", "#377EB8", "#4DAF4A", 
                              "#984EA3", "#FF7F00", "#FFFF33"), name=NULL) + 
   coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
   guides(colour=guide_legend(order=1),
          fill=guide_legend(order=2)) +
-  labs(x=NULL, y="Presidents and wars") +
+  labs(x=NULL, y=NULL) +
   theme_ath() +
   theme(legend.position="bottom", 
         panel.grid.major.y=element_blank(),
         panel.grid.minor.y=element_blank(),
         legend.key.size=unit(0.65, "lines"),
         legend.key=element_blank(), legend.margin=unit(0.25, "lines"),
-        axis.text.y=element_blank())
+        axis.text=element_blank())
 
 plot.laws <- ggplot(laws, aes(x=Date, y=0)) + 
-  geom_point(size=3) +
-  geom_text_repel(data=laws, aes(x=Date, y=0, label=law), 
-            size=2.5, point.padding=unit(0.5, "lines"),
-            box.padding=unit(0.75, "lines"),
-            family="Source Sans Pro Semibold") + 
-  coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31"))) +
+  geom_segment(aes(x=Date, xend=Date, y=0, yend=plot_y), 
+               size=0.5, colour="grey30") +
+  geom_label(data=laws,
+            aes(x=Date, y=plot_y, label=law, hjust=plot_h),
+            family="Source Sans Pro Semibold", size=2.5,
+            fill="grey30", colour="white", label.padding=unit(0.3, "lines")) +
+  geom_point(size=3, colour="grey30") +
+  coord_cartesian(xlim=ymd(c("1995-01-01", "2015-12-31")),
+                  ylim=c(-1, 3)) +
   labs(x=NULL, y=NULL) +
   theme_ath() + 
-  theme(panel.grid.major.y=element_blank(),
-        panel.grid.minor.y=element_blank(),
-        axis.text.y=element_blank())
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        axis.text=element_blank())
+plot.laws
 
-# Combine all the plots (wihout gridExtra! Yay gtable!)
+# Combine all the plots
 plot.timeline <- rbind(ggplotGrob(plot.laws),
                        ggplotGrob(plot.csre),
                        ggplotGrob(plot.icrg.internal),
@@ -117,9 +128,9 @@ plot.timeline <- rbind(ggplotGrob(plot.laws),
 # Adjust panel sizes
 # via http://stackoverflow.com/a/24333504/120898
 panels <- plot.timeline$layout$t[grep("panel", plot.timeline$layout$name)]
-plot.timeline$heights[panels] <- lapply(c(0.5, 1, 1, 1, 1, 0.25), unit, "null")
+plot.timeline$heights[panels] <- lapply(c(0.9, 1, 1, 1, 1, 0.15), unit, "null")
 
 grid::grid.draw(plot.timeline)
 
 fig.save.cairo(plot.timeline, filename="1-russia-timeline", 
-               width=6, height=7)
+               width=6, height=7.5)
