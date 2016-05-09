87447b224b7f45d825a2c8afef82bcac9fc3804b
Andrew Heiss <andrew@andrewheiss.com>
2016-05-09 11:28:29 -0400

    Miscellaneous fake SQL queries with dplyr

---
+++ Data/Survey/faux_sql.R
@@ -0,0 +1,233 @@
+library(dplyr)
+library(readr)
+library(stringr)
+
+
+# ----------------------------------------------------------
+# Clean bounces, blocks, unsubscribes, and invalid e-mails
+# ----------------------------------------------------------
+db.email <- src_sqlite(path=file.path(PROJHOME, "Data", "Survey", "list", 
+                                      "final_list.db"))
+removed <- tbl(db.email, "remove") %>% collect()
+bounced <- tbl(db.email, "bounces") %>% collect()
+email.full <- tbl(db.email, "full_list") %>% collect() %>%
+  rowwise() %>%
+  mutate(domain = str_match(email, "@(.*)$")[2]) %>%
+  ungroup()
+
+blocks.raw <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                      "suppressions", "suppression_blocks.csv")) %>%
+  mutate(dead_domain = ifelse(str_detect(reason, "MX"), TRUE, FALSE))
+
+# Dead domains
+dead.domains <- blocks.raw %>%
+  filter(dead_domain == TRUE) %>%
+  rowwise() %>%
+  mutate(domain = str_match(email, "@(.*)$")[2]) %>%
+  select(dead_domain, domain) %>% 
+  ungroup()
+
+ids.to.remove.dead.domain <- email.full %>%
+  mutate(dead_domain = ifelse(domain %in% dead.domains$domain, TRUE, FALSE)) %>%
+  filter(dead_domain == TRUE) %>%
+  filter(!(index_org %in% removed$fk_org))
+
+sql.command <- ids.to.remove.dead.domain %>%
+  mutate(remove = 1, remove_notes = "Domain is dead; has no mail server") %>%
+  select(fk_org = index_org, remove, remove_notes)
+write_csv(sql.command, 
+          file.path(PROJHOME, "Data", "Survey",
+                    "sql_csvs", "suppression_blocks.csv"))
+
+
+# Bounces (in both blocks.raw and bounces.raw)
+bounces.blocks <- blocks.raw %>% filter(dead_domain == FALSE) %>% select(-dead_domain)
+bounces.raw <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                       "suppressions", "suppression_bounces.csv"))
+
+bounces.all <- bind_rows(bounces.raw, bounces.blocks) %>%
+  left_join(select(email.full, index_org, email), by="email")
+
+bounces.unknown <- bounces.all %>% filter(is.na(index_org))
+
+ids.to.remove.bounces <- email.full %>%
+  filter(index_org %in% bounces.all$index_org) %>%
+  filter(!(index_org %in% bounced$fk_org))
+
+sql.command <- ids.to.remove.bounces %>%
+  mutate(hard_bounce = 1, email_dead = 0, email_bounce = 0) %>%
+  select(fk_org = index_org, hard_bounce, email_dead, email_bounce)
+write_csv(sql.command, 
+          file.path(PROJHOME, "Data", "Survey",
+                    "sql_csvs", "bounces.csv"))
+
+
+# Unsubcribed
+unsub.raw <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                     "suppressions", "suppression_unsubscribes.csv"))
+
+ids.to.remove.unsub <- email.full %>%
+  filter(email %in% unsub.raw$email) %>%
+  filter(!(index_org %in% removed$fk_org))
+
+sql.command <- ids.to.remove.unsub %>%
+  mutate(remove = 1, remove_notes = "Unsubscribed") %>%
+  select(fk_org = index_org, remove, remove_notes)
+write_csv(sql.command, 
+          file.path(PROJHOME, "Data", "Survey",
+                    "sql_csvs", "unsubscribes.csv"))
+
+
+# Invalid addresses
+invalid.raw <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                       "suppressions", "suppression_invalid_emails.csv"))
+
+ids.to.remove.invalid <- email.full %>%
+  filter(email %in% invalid.raw$email) %>%
+  filter(!(index_org %in% removed$fk_org))
+
+sql.command <- ids.to.remove.invalid %>%
+  mutate(remove = 1, remove_notes = "Invalid address") %>%
+  select(fk_org = index_org, remove, remove_notes)
+write_csv(sql.command, 
+          file.path(PROJHOME, "Data", "Survey",
+                    "sql_csvs", "invalid.csv"))
+
+
+# -----------------------------
+# Export email sending groups
+# -----------------------------
+db.email <- src_sqlite(path=file.path(PROJHOME, "Data", "Survey", "list", 
+                                      "final_list.db"))
+email.full <- tbl(db.email, "full_list") %>% collect()
+removed <- tbl(db.email, "remove") %>% collect()
+bounced <- tbl(db.email, "bounces") %>% collect()
+completed <- tbl(db.email, "survey_completed") %>% collect()
+
+# Super clean e-mails from Email Hippo
+hippo.raw <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                     "suppressions", "hippo_check.csv"))
+
+hippo.clean <- hippo.raw %>% filter(Status == "Ok")
+
+
+current.group <- 28
+
+ids.to.skip <- c(removed$fk_org, bounced$fk_org, completed$fk_org)
+ids.clean <- hippo.clean$index_org
+
+to.send <- email.full %>%
+  filter(!(index_org %in% ids.to.skip)) %>%
+  filter(index_org %in% ids.clean) %>%
+  filter(group == current.group) %>%
+  mutate(email = strsplit(as.character(email), ",")) %>%
+  unnest(email) %>%
+  select(index_org, id_org, org_name_email, email, group)
+
+filename <- paste0("round_", current.group, "_super_clean.csv")
+write_csv(to.send, path=file.path(PROJHOME, "Data", "Survey", "list",
+                                  "groups_clean_hippo", filename),
+          na="")
+
+# all.groups <- email.full %>%
+#   filter(!(index_org %in% ids.to.skip)) %>%
+#   filter(!(index_org %in% to.send.hippo$id_org)) %>%
+#   filter(!(group %in% as.character(1:8))) %>%
+#   mutate(email = strsplit(as.character(email), ",")) %>%
+#   unnest(email) %>%
+#   select(index_org, id_org, org_name_email, email, group)
+# write_csv(all.groups, "~/Desktop/hippo_check.csv")
+
+
+# # Email Hippo
+# hippo <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+#                                  "suppressions", "hippo_check.csv"))
+# 
+# hippo.dead <- hippo %>%
+#   filter(Status == "Bad") %>%
+#   filter(AdditionalStatusInfo != "MailboxDoesNotExist") 
+# 
+# ids.to.remove.hippo.dead <- email.full %>%
+#   filter(index_org %in% hippo.dead$index_org) %>%
+#   filter(!(index_org %in% bounced$fk_org)) %>%
+#   filter(!(index_org %in% removed$fk_org))
+# 
+# hippo.sql.dead <- ids.to.remove.hippo.dead %>%
+#   mutate(remove = 1, remove_notes = "Hippo: domain dead") %>%
+#   select(fk_org = index_org, remove, remove_notes)
+# write_csv(hippo.sql.dead, "~/Desktop/mail_cleaning/hippo_domain_dead.csv")
+# 
+# hippo.email.dead <- hippo %>%
+#   filter(Status == "Bad") %>%
+#   filter(AdditionalStatusInfo == "MailboxDoesNotExist") 
+# 
+# ids.to.remove.hippo.email.dead <- email.full %>%
+#   filter(index_org %in% hippo.email.dead$index_org) %>%
+#   filter(!(index_org %in% bounced$fk_org)) %>%
+#   filter(!(index_org %in% removed$fk_org))
+# 
+# hippo.sql.dead.email <- ids.to.remove.hippo.email.dead %>%
+#   mutate(remove = 1, remove_notes = "Hippo: e-mail doesn't exist") %>%
+#   select(fk_org = index_org, remove, remove_notes)
+# write_csv(hippo.sql.dead.email, "~/Desktop/mail_cleaning/hippo_email_dead.csv")
+
+# hippo.clean <- hippo %>%
+#   filter(Status != "Bad")
+# write_csv(hippo.clean, "~/Desktop/round_9_clean_hippo_clean.csv")
+
+
+# ------------------------
+# 1st round of reminders
+# ------------------------
+db.email <- src_sqlite(path=file.path(PROJHOME, "Data", "Survey", "list", 
+                                      "final_list.db"))
+email.full <- tbl(db.email, "full_list") %>% collect()
+removed <- tbl(db.email, "remove") %>% collect()
+bounced <- tbl(db.email, "bounces") %>% collect()
+completed <- tbl(db.email, "survey_completed") %>% collect()
+
+wants.to.call <- c("25907", "20653", "20558", "19362", "727", "22348", "26229", "19737", "329", "22767", "24060", "2122", "24904", "19868", "18664", "18606", "6993", "24892", "18556", "18437", "20202", "18244", "17770", "17876", "20368", "18894", "24338", "83")
+
+ids.to.skip <- c(removed$fk_org, bounced$fk_org, completed$fk_org, wants.to.call)
+
+reminders <- email.full %>%
+  filter(!(index_org %in% ids.to.skip)) %>%
+  filter(group %in% as.character(1:9)) %>%
+  mutate(email = strsplit(as.character(email), ",")) %>%
+  unnest(email) %>%
+  select(index_org, id_org, org_name_email, email, group)
+
+# Check these at Email Hippo
+#
+#       $$$$$$$$$$$$
+#       $          $
+#       $    :(    $
+#       $          $
+#       $$$$$$$$$$$$
+#
+write_csv(reminders, path="~/Desktop/early_groups.csv")
+
+
+# Filter out dead addresses
+hippo.early <- read_csv(file=file.path(PROJHOME, "Data", "Survey",
+                                       "suppressions", "hippo_early_groups.csv"))
+ 
+hippo.clean <- hippo.early %>% filter(Status == "Ok")
+
+reminders.1.8 <- reminders %>%
+  filter(index_org %in% hippo.clean$index_org) %>%
+  filter(group %in% as.character(1:8))
+
+
+# Save individual group lists
+writer <- function(df) {
+  filename <- paste0("group_", unique(df$group), "_clean.csv")
+  readr::write_csv(df, path=file.path(PROJHOME, "Data", "Survey", "list", 
+                                      "groups_reminders", filename),
+                   na="")
+  return(df)
+}
+
+reminders.1.8 %>%
+  group_by(group) %>%
+  do(writer(.))
---
+++ Data/Survey/match_results.R
@@ -0,0 +1,80 @@
+library(dplyr)
+library(readr)
+library(feather)
+
+countries <- read_feather(file.path(PROJHOME, "Data", "Survey", "output",
+                                    "survey_countries.feather"))
+
+# Pilot-ish survey
+# Qualtrics isn't letting me download the full raw CSV file, so this is a
+# subset, which uses a slightly different structure and needs to be rearranged
+# results.colnames <- c("Q2.1", "Q2.2", "Q2.4", "ResponseID", "EndDate", "Finished", "Q5.4")
+# 
+# survey.raw <- read_csv(results.file, col_names=results.colnames, skip=2)
+# 
+# responses.matching <- survey.raw %>%
+#   select(ResponseID, EndDate, Finished, Q2.1, Q2.2, Q2.4, Q5.4) %>%
+#   arrange(EndDate)
+
+results.file <- file.path(PROJHOME, "Data", "Survey", "raw_data", 
+                          "INGOs_and_government_regulations.csv")
+
+results.colnames <- read_csv(results.file, col_names=FALSE, n_max=1) %>%
+  c %>% unlist %>% unname
+
+survey.raw <- read_csv(results.file, col_names=results.colnames, skip=3)
+
+responses.matching.pilot <- survey.raw %>%
+  mutate(Finished = Finished == 1,
+         Q2.4 = Q2.4 == 1) %>%
+  left_join(select(countries, c(`Country name`, `Qualtrics ID`)),
+            by=c("Q2.2" = "Qualtrics ID")) %>%
+  select(ResponseID, EndDate, Finished, `Organization name` = Q2.1, `Country name`, 
+         `Is INGO` = Q2.4, `Contact` = Q5.4)
+
+# Final survey
+results.file <- file.path(PROJHOME, "Data", "Survey", "raw_data", 
+                          "INGOs_and_government_regulations_new.csv")
+
+results.colnames <- read_csv(results.file, col_names=FALSE, n_max=1) %>%
+  c %>% unlist %>% unname
+
+survey.raw <- read_csv(results.file, col_names=results.colnames, skip=3)
+
+responses.matching.full <- survey.raw %>%
+  mutate(Finished = Finished == 1,
+         Q2.4 = Q2.4 == 1) %>%
+  left_join(select(countries, c(`Country name`, `Qualtrics ID`)),
+            by=c("Q2.2" = "Qualtrics ID")) %>%
+  select(ResponseID, EndDate, Finished, `Organization name` = Q2.1, `Country name`, 
+         `Is INGO` = Q2.4, `Contact` = Q5.4)
+
+responses.matching <- bind_rows(responses.matching.pilot,
+                                responses.matching.full)
+
+responses.matching.completed <- responses.matching %>%
+  filter(Finished == TRUE)
+
+
+# Load database and get list of already-recorded surveys
+db.email <- src_sqlite(path=file.path(PROJHOME, "Data", "Survey", "list", 
+                                      "final_list.db"))
+email.full <- tbl(db.email, "full_list") %>% collect()
+completed <- tbl(db.email, "survey_completed") %>% collect()
+
+unknown.completed <- c("R_1oA250tKpKvZZeW", "R_1Kyc9UBAFa8zIOE", "R_3qNL7uZ5gkeqykd", "R_1JWode0p5e0IGn6", "R_aY33zkGNKQxKUeh", "R_2ydDp0ToXr9xpXN", "R_3qeXqHoJbHAJV7T", "R_9zQxb03X6UT81Wh", "R_3qlpCKseYkkTO5s", "R_ABz6acut27BE0xz", "R_3I4677WoGBOmW4Q", "R_1I3H1V2bIjObFUm", "R_50YS1E8qFvzh5yt")
+
+responses.to.be.matched <- responses.matching.completed %>%
+  filter(!(ResponseID %in% c(completed$qualtrics_id, unknown.completed)))
+
+write_csv(responses.to.be.matched,
+          file.path(PROJHOME, "Data", "Survey", "output",
+                    "responses_to_be_matched.csv"))
+
+# Lookup table
+org.lookup <- email.full %>%
+  select(index_org, id_org, org_name, email, org_url, country_hq)
+
+write_csv(org.lookup,
+          file.path(PROJHOME, "Data", "Survey", "output",
+                    "org_lookup.csv"))
