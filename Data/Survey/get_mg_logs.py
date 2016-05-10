c81077f2a9ebad7762d01a00e65cb0c39208550d
Andrew Heiss <andrew@andrewheiss.com>
2016-05-10 01:25:00 -0400

    Export Mailgun bounces and unsubscribes to CSV

--- Data/Survey/get_mg_logs.py
+++ Data/Survey/get_mg_logs.py
@@ -2,6 +2,7 @@
 import mail_config
 import requests
 import json
+import csv
 
 def get_bounces():
     return requests.get(
@@ -15,6 +16,17 @@ def get_unsubscribes():
         auth=('api', mail_config.MG_API_KEY),
         params={'limit': 1000})
 
-all_unsubs = json.loads(get_unsubscribes().text)
 
-print(all_unsubs['items'])
+all_unsubs = json.loads(get_unsubscribes().text)
+with open('suppressions/mg_unsubscribes.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerow(['email'])
+    for row in all_unsubs['items']:
+        w.writerow([row['address']])
+
+all_bounces = json.loads(get_bounces().text)
+with open('suppressions/mg_bounces.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerow(['email'])
+    for row in all_bounces['items']:
+        w.writerow([row['address']])
