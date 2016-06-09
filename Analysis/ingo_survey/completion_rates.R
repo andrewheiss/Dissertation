29292780b096942ebfeca6c6939e7e3c3031d157
Andrew Heiss <andrew@andrewheiss.com>
2016-06-08 20:35:13 -0400

    First commit

---
+++ Analysis/ingo_survey/completion_rates.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+# Absorption rate
+# Quality of the list
+
+# Break-off rate
+
+# Completion rate
+
+# Study-specific screening completion rates
+
+# Study-specific eligibility rate
