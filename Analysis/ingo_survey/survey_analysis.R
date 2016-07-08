07ae09259ad7aa6f324356b9e3ed5600bb37952d
Andrew Heiss <andrew@andrewheiss.com>
2016-07-08 14:23:58 -0400

    Preliminary questions

---
+++ Analysis/ingo_survey/survey_analysis.R
@@ -0,0 +1,88 @@
+#' ---
+#' title: "Survey analysis"
+#' author: "Andrew Heiss"
+#' date: "`r format(Sys.time(), '%B %e, %Y')`"
+#' output: 
+#'   html_document: 
+#'     css: ../html/fixes.css
+#'     toc: yes
+#'     highlight: pygments
+#'     theme: cosmo
+#'     includes:
+#'       after_body: ../html/jump.html
+#'     keep_md: yes
+#' bibliography: /Users/andrew/Dropbox/Readings/Papers.bib
+#' csl: /Users/andrew/.pandoc/csl/american-political-science-association.csl
+#' ...
+
+#' # Load clean data and set everything up
+#+ message=FALSE
+library(dplyr)
+library(ggplot2)
+library(ggstance)
+library(pander)
+
+panderOptions('table.split.table', Inf)
+panderOptions('table.split.cells', Inf)
+panderOptions('keep.line.breaks', TRUE)
+panderOptions('table.style', 'multiline')
+panderOptions('table.alignment.default', 'left')
+
+source(file.path(PROJHOME, "Analysis", "lib", "graphic_functions.R"))
+
+# Load cleaned, country-based survey data (*with* the Q4\* loop)
+survey.clean.all <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                      "survey_clean_all.rds"))
+
+# Load cleaned, organization-based data (without the Q4 loop)
+survey.orgs.clean <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                       "survey_orgs_clean.rds"))
+
+# Load cleaned, country-based data (only the Q4 loop)
+survey.countries.clean <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                            "survey_countries_clean.rds"))
+
+
+#' # General questions
+#' 
+#'
+#' How are these NGOs distributed by HQ?
+#' 
+#' Where do these NGOs work? Count all the countries they selected + the countries they answered for
+#'
+#' Who responded in the organizations?
+#' 
+#' ## What do these NGOs do?
+#' 
+#' What kind of issues do the NGOs work on? Which issues do they work on the most?
+#' 
+#' What kinds of activities do these NGOs engage in?
+#' 
+#' How big are these NGOs? Staff? Volunteers?
+#' 
+#' Do NGOs collaborate?
+#' 
+#' Where does their funding come from?
+#' 
+#' ## Deeper principles (mission, vision, values)
+#' 
+#' Q3.9 - Q3.13
+
+#' # Testing hypotheses
+#' 
+#' My claim:
+#' 
+#' > If an INGO does not make one of these three adjustments—shifting its ideal
+#' point, increasing its operational flexibility, or becoming essential for the
+#' regime—it runs a high risk of expulsion.
+#' 
+#' What influences feelings of restriction? Main hypotheses:
+#' 
+#' - Normative principles and ideals
+#'     - Issue area
+#' - Instrumental flexibility
+#'     - Size
+#'     - Type of funding
+#'     - Collaboration
+#' - Alignment with regime preferences
+#' 
