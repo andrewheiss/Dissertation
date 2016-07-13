0e1c0911d23bccf89b32136b6c4dbf8ac3cfff91
Andrew Heiss <andrew@andrewheiss.com>
2016-07-13 12:51:40 -0400

    Start *actual* survey analysis
    Not just the summary…

---
+++ Analysis/ingo_survey/survey_analysis.R
@@ -0,0 +1,21 @@
+#' ---
+#' title: "Survey analysis"
+#' author: "Andrew Heiss"
+#' date: "`r format(Sys.time(), '%B %e, %Y')`"
+#' output: 
+#'   html_document: 
+#'     css: ../html/fixes.css
+#'     code_folding: hide
+#'     toc: yes
+#'     toc_float: true
+#'     toc_depth: 4
+#'     highlight: pygments
+#'     theme: cosmo
+#'     keep_md: yes
+#' bibliography: /Users/andrew/Dropbox/Readings/Papers.bib
+#' csl: /Users/andrew/.pandoc/csl/american-political-science-association.csl
+#' ...
+
+#' ## Load clean data
+#+ message=FALSE
+knitr::opts_chunk$set(fig.retina=2)
