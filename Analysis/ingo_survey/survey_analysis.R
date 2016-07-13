fcec110b36b2746a2e526f5dc854d8f22dfbb31f
Andrew Heiss <andrew@andrewheiss.com>
2016-07-13 12:59:35 -0400

    Load data

--- Analysis/ingo_survey/survey_analysis.R
+++ Analysis/ingo_survey/survey_analysis.R
@@ -19,3 +19,44 @@
 #' ## Load clean data
 #+ message=FALSE
 knitr::opts_chunk$set(fig.retina=2)
+
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(ggplot2)
+library(ggstance)
+library(stringr)
+library(pander)
+library(magrittr)
+library(DT)
+library(scales)
+library(countrycode)
+library(tm)
+
+panderOptions('table.split.table', Inf)
+panderOptions('table.split.cells', Inf)
+panderOptions('keep.line.breaks', TRUE)
+panderOptions('table.style', 'multiline')
+panderOptions('table.alignment.default', 'left')
+
+source(file.path(PROJHOME, "Analysis", "lib", "graphic_functions.R"))
+
+# Load cleaned, country-based survey data (*with* the Q4\* loop)
+survey.clean.all <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                      "survey_clean_all.rds"))
+
+# Load cleaned, organization-based data (without the Q4 loop)
+survey.orgs.clean <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                       "survey_orgs_clean.rds"))
+
+# Load cleaned, country-based data (only the Q4 loop)
+survey.countries.clean <- readRDS(file.path(PROJHOME, "Data", "data_processed", 
+                                            "survey_countries_clean.rds"))
+
+# Load Robinson map projection
+countries.ggmap <- readRDS(file.path(PROJHOME, "Data", "data_processed",
+                                     "countries110_robinson_ggmap.rds"))
+
+# All possible countries (to fix the South Sudan issue)
+possible.countries <- data_frame(id = unique(as.character(countries.ggmap$id)))
+
