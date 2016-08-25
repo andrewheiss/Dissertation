1c0bf6e8c67e6cc149203d09a84f73976f58dc6f
Andrew Heiss <andrew@andrewheiss.com>
2016-08-25 09:43:09 -0400

    Add stat_chull

--- Analysis/lib/graphic_functions.R
+++ Analysis/lib/graphic_functions.R
@@ -173,3 +173,23 @@ fig.coef <- function(models, title=NULL, xlab=NULL, legend=TRUE,
   }
   coef.plot
 }
+
+# ggplot layer for the convex hull of a set of points
+# http://docs.ggplot2.org/dev/vignettes/extending-ggplot2.html#the-simplest-stat
+StatChull <- ggproto("StatChull", Stat,
+                     compute_group = function(data, scales) {
+                       data[chull(data$x, data$y), , drop = FALSE]
+                     },
+                     
+                     required_aes = c("x", "y")
+)
+
+stat_chull <- function(mapping = NULL, data = NULL, geom = "polygon",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatChull, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
